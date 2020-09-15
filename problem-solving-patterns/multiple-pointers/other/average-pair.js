ff9c0ade9c8834f1d6a1fadcbc04df5b47450196
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-09-15 11:51:38 +0100

    feat: Add solution for average pair problem

---
+++ problem-solving-patterns/multiple-pointers/other/average-pair.js
@@ -0,0 +1,22 @@
+// Given a sorted array of integers and a target average,
+// is there a pair of values in the array where the average of the pair equals the target average
+
+function averagePair(arr, target){
+  let start = 0
+  let end = arr.length-1
+
+  while (start < end) {
+    let avg = (arr[start] + arr[end]) / 2
+    if (avg === target) {
+      return true
+    } else if (avg < target) {
+      start++
+    } else {
+      end--
+    }
+  }
+
+  return false
+}
+
+console.log(averagePair([1,2,3], 2.5))
