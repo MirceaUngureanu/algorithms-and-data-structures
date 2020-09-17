bb3ae010cf486e906745893d2882640e63f2259c
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-09-17 15:01:57 +0100

    feat: Solution for "capitalise words"

---
+++ recursion/problems/capitalizeWords.js
@@ -0,0 +1,14 @@
+// O(N)
+// Given an array of words, return a new array containing each word capitalized
+
+function capitalizeWords(array) {
+  if (array.length === 1) {
+    return [array[0].toUpperCase()];
+  }
+  let res = capitalizeWords(array.slice(0, -1));
+  res.push(array.slice(array.length-1)[0].toUpperCase());
+  return res;
+
+}
+
+console.log(capitalizeWords(['i', 'am', 'learning', 'recursion']))
