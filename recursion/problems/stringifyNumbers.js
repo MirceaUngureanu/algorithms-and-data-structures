46e484a217ea5f3a1817f6448c53f63e43416688
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-09-17 15:06:04 +0100

    feat: Solution for "stringify numbers"

---
+++ recursion/problems/stringifyNumbers.js
@@ -0,0 +1,30 @@
+// O(N)
+// Given an object, find all values which are numbers and convert them to strings
+
+let obj = {
+  num: 1,
+  test: [],
+  data: {
+    val: 4,
+    info: {
+      isRight: true,
+      random: 66
+    }
+  }
+}
+
+function stringifyNumbers(obj) {
+  let newObj = {}
+  for (let key in obj) {
+    if (typeof obj[key] === 'number') {
+      newObj[key] = obj[key].toString();
+    } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+      newObj[key] = stringifyNumbers(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+}
+
+console.log(stringifyNumbers(obj))
