ed5974e667430193d95040f014b817290c1700a7
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-09-18 09:12:23 +0100

    feat: Add "naive string search"

---
+++ searching-algorithms/naiveStringSearch.js
@@ -0,0 +1,24 @@
+// O(N^2)
+// Given a longer string and a smaller string, count the nr of times the smaller string appears in the longer string
+// wowomgzomg
+//  omg
+
+
+function stringSearch(larger, smaller) {
+  let count = 0
+
+  for (let i = 0; i < larger.length; i++) {
+    for (let j = 0; j < smaller.length; j++) {
+      // string doesn't match
+      if (smaller[j] !== larger[i+j]) break
+      // j is the is at the last index, full string match
+      if (j === smaller.length - 1) {
+        count++
+      }
+    }
+  }
+
+  return count
+}
+
+console.log(stringSearch("wowomgzomg", "omg"))
