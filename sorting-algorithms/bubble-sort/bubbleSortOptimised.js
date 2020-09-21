8f9c8c02d220785bf20b6d87c7dbc74c0b65e203
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-09-21 11:05:07 +0100

    feat: Add bubble sort solutions

---
+++ sorting-algorithms/bubble-sort/bubbleSortOptimised.js
@@ -0,0 +1,24 @@
+// Optimized BubbleSort with noSwaps
+// Still O(N^2) worst case scenario, but closer to O(N) when used for nearly sorted data
+// due to removal of unnecessary actions
+function bubbleSort(arr){
+  let noSwaps;
+
+  for(let i = arr.length; i > 0; i--){
+    noSwaps = true;
+
+    for(let j = 0; j < i - 1; j++){
+      if(arr[j] > arr[j+1]){
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+        noSwaps = false;
+      }
+    }
+    // no swaps on last inner loop
+    if(noSwaps) break;
+  }
+  return arr;
+}
+
+console.log(bubbleSort([8,1,2,3,4,5,6,7]))
---
+++ sorting-algorithms/bubble-sort/bubbleSortUnoptimised.js
@@ -0,0 +1,36 @@
+// UNOPTIMIZED VERSION OF BUBBLE SORT
+// O(N^2)
+function bubbleSort(arr){
+  // start from the end so we don't have unnecessary operations
+  for(let i = arr.length; i > 0; i--){
+    // only loop the required number of times for remaining items
+    for(let j = 0; j < i - 1; j++){
+      console.log(arr, arr[j], arr[j+1]);
+      if(arr[j] > arr[j+1]){
+        // sort array
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// ES2015 Version
+// function bubbleSortES6(arr) {
+//   const swap = (arr, idx1, idx2) => {
+//     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+//   };
+//
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         swap(arr, j, j + 1);
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+console.log(bubbleSort([8,1,2,3,4,5,6,7]))
