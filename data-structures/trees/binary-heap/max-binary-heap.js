779a68bb1902013b21365f1bcda414d846220c6e
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-10-10 19:38:35 +0100

    feat: Add solution for ExtractMax

--- data-structures/trees/binary-heap/max-binary-heap.js
+++ data-structures/trees/binary-heap/max-binary-heap.js
@@ -21,6 +21,56 @@ class MaxBinaryHeap {
       idx = parentIdx;
     }
   }
+
+  extractMax() {
+    const max = this.values[0]
+    const end = this.values.pop()
+
+    if (this.values.length > 0) {
+      this.values[0] = end
+      this.sinkDown()
+    }
+
+    console.log(this.values)
+    return max
+  }
+
+  sinkDown() {
+    let idx = 0
+    const length = this.values.length
+    const element = this.values[0]
+
+    while (true) {
+      let leftChildIdx = 2 * idx + 1
+      let rightChildIdx = 2 * idx + 2
+      let leftChild,
+          rightChild
+      let swap = null
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx]
+        if (leftChild > element) {
+          swap = leftChildIdx
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx]
+
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx
+        }
+      }
+
+      if (swap === null) break
+      this.values[idx] = this.values[swap]
+      this.values[swap] = element
+      idx = swap
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap();
@@ -33,4 +83,5 @@ heap.insert(12);
 heap.insert(55);
 
 console.log(heap.insert(199))
+console.log(heap.extractMax())
 
