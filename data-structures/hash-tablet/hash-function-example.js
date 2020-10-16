1b83d0004ff6147bd495a2d657c7561668600301
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-10-16 16:43:38 +0100

    feat: Add hash function example

---
+++ data-structures/hash-tablet/hash-function-example.js
@@ -0,0 +1,14 @@
+function hash(key, arrayLen) {
+  let total = 0;
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charCodeAt(0) - 96
+    total = (total * WEIRD_PRIME + value) % arrayLen;
+  }
+  return total;
+}
+
+console.log(hash("hello", 13))
+console.log(hash("pink", 13))
+console.log(hash("cyan", 13))
